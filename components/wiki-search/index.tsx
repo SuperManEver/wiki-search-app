88b385cf8a8a476ac177bf3fd5927af89163aa91
Nik Luparev <nikita.luparev@gmail.com>
2024-01-23 17:09:58 +0500

    progress on calculating height

--- components/wiki-search/index.tsx
+++ components/wiki-search/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 // vendor
-import { useState, ChangeEvent, KeyboardEvent, useRef } from 'react'
+import { useState, ChangeEvent, KeyboardEvent, useRef, useEffect } from 'react'
 import { useVirtualizer } from '@tanstack/react-virtual'
 
 // types
@@ -23,8 +23,14 @@ interface IProps {
   className?: string
 }
 
+const MARGIN = 24
+
 function WikiSearch({ className }: IProps) {
+  const rootRef = useRef<HTMLDivElement>(null)
+  const searchBlockRef = useRef<HTMLDivElement>(null)
   const parentRef = useRef<HTMLDivElement>(null)
+  const [divHeight, setDivHeight] = useState<number>(0)
+
   const [query, setQuery] = useState<string>('')
   const {
     data = [],
@@ -76,9 +82,34 @@ function WikiSearch({ className }: IProps) {
    * https://stackoverflow.com/a/44109531
    */
 
+  useEffect(() => {
+    if (!rootRef || !searchBlockRef) {
+      return
+    }
+
+    if (rootRef.current && searchBlockRef.current) {
+      const { height: parentHeight } = rootRef.current.getBoundingClientRect()
+      const { height: searchHeight } =
+        searchBlockRef.current.getBoundingClientRect()
+
+      const availableSpace = Math.floor(parentHeight - searchHeight - MARGIN)
+
+      if (availableSpace > 0) {
+        setDivHeight(availableSpace)
+      }
+
+      console.log(
+        'we have current: ',
+        parentHeight,
+        searchHeight,
+        availableSpace
+      )
+    }
+  }, [])
+
   return (
-    <div className={className}>
-      <div className={css.searchWrapper}>
+    <div className={className} ref={rootRef}>
+      <div ref={searchBlockRef} className={css.searchWrapper}>
         <SearchInput
           value={query}
           onChange={handleQueryChange}
@@ -91,49 +122,47 @@ function WikiSearch({ className }: IProps) {
         </Button>
       </div>
 
-      <section className={css.listWrapper}>
-        {loading ? (
-          <div className={css.loadingContainer}>
-            <Spinner />
-          </div>
-        ) : (
+      {loading ? (
+        <div className={css.loadingContainer}>
+          <Spinner />
+        </div>
+      ) : (
+        <div
+          ref={parentRef}
+          className={css.scrollWrapper}
+          style={{
+            height: `${divHeight}px`, // THIS IS IMPORTANT 62vh
+            overflow: 'auto',
+          }}
+        >
           <div
-            ref={parentRef}
-            className={css.scrollWrapper}
             style={{
-              height: '62vh', // THIS IS IMPORTANT
-              overflow: 'auto',
+              height: `${totalSize}px`,
+              width: '100%',
+              position: 'relative',
             }}
           >
-            <div
-              style={{
-                height: `${totalSize}px`,
-                width: '100%',
-                position: 'relative',
-              }}
-            >
-              {virtualItems.map((vItem) => {
-                const item = data[vItem.index]
+            {virtualItems.map((vItem) => {
+              const item = data[vItem.index]
 
-                return (
-                  <SearchItem
-                    key={item.id}
-                    item={item}
-                    style={{
-                      position: 'absolute',
-                      top: 0,
-                      left: 0,
-                      width: '100%',
-                      height: `${vItem.size}px`,
-                      transform: `translateY(${vItem.start}px)`,
-                    }}
-                  />
-                )
-              })}
-            </div>
+              return (
+                <SearchItem
+                  key={item.id}
+                  item={item}
+                  style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: `${vItem.size}px`,
+                    transform: `translateY(${vItem.start}px)`,
+                  }}
+                />
+              )
+            })}
           </div>
-        )}
-      </section>
+        </div>
+      )}
     </div>
   )
 }
